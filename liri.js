99f6d58662df1870c6d5162f4bfdbefbb01ca517
Noel Nevarez <noel3740@gmail.com>
2018-12-07 11:28:38 -0600

    Adding function code for the "spotify-this-song" command.

--- liri.js
+++ liri.js
@@ -55,6 +55,7 @@ function logMessage(message) {
     console.log(message);
 }
 
+//Function to run when the "concert-this" command is used
 function concertThis() {
 
     var artist = process.argv[3];
@@ -69,23 +70,24 @@ function concertThis() {
         //Build the query url to bands in town api
         var queryUrl = `https://rest.bandsintown.com/artists/${encodeURI(artist)}/events?app_id=codingbootcamp`
 
-        console.log("queryUrl", queryUrl);
         // Create a request with axios to the queryUrl
         axios.get(queryUrl).then(
             function (response) {
-                
+
                 //Build out a string for the message that we'll log to the console and in a text file
                 var messageArray = [];
 
                 messageArray.push("=====================================");
                 messageArray.push(`"Concert This" results for ${artist}`);
                 messageArray.push("=====================================");
-                
+
                 //Loop thru each event we received in the response data
-                 response.data.forEach(event => {
+                response.data.forEach(event => {
                     //Add the details of the venue to the message array
-                    messageArray.push(`Name of venue: ${event.venue.name}, Venue location: ${event.venue.city}, ${event.venue.region}, ${event.venue.country}, Date: ${moment(event.datetime).format("MM/DD/YYYY")}`)
-                }); 
+                    messageArray.push(`Date: ${moment(event.datetime).format("MM/DD/YYYY")}`);
+                    messageArray.push(`\t Name of Venue: ${event.venue.name}`);
+                    messageArray.push(`\t Venue Location: ${event.venue.city}, ${event.venue.region ? event.venue.region + ", " : ""}${event.venue.country}`);
+                });
 
                 messageArray.push("=====================================");
 
@@ -96,14 +98,78 @@ function concertThis() {
     }
 }
 
+//Function to run when the "spotify-this-song" command is used
 function spotifyThisSong() {
 
+    //Get the song name passed in as a parameter
+    //If the song name was not passed then use default
+    var songName = process.argv[3] ? process.argv[3] : "The Sign";
+
+    //Use the spotify api package to search for the song the user entered
+    spotify
+        .search({ type: 'track', query: songName })
+        .then(function (response) {
+
+            //Build out a string for the message that we'll log to the console and in a text file
+            var messageArray = [];
+
+            messageArray.push("=====================================");
+            messageArray.push(`"Spotify This Song" results for "${songName}"`);
+            messageArray.push("=====================================");
+
+            //Sort the results by song name
+            var sortedResults = response.tracks.items.sort(spotifyItemSortBySongName);
+
+            //Loop thru all the tracks that we received and push the details of each track to the message array
+            sortedResults.forEach(track => {
+
+                //console.log(track);
+
+                //Loop thru the artists and build out the artist string
+                var artists = "";
+                track.artists.forEach(artist => {
+                    artists += artist.name + ",";
+                });
+
+                messageArray.push(`Song Name: ${track.name}`);
+                messageArray.push(`\t Artist(s): ${artists}`); 
+                messageArray.push(`\t Album ${track.album.name}`);
+                messageArray.push(`\t Preview Link: ${track.preview_url}`);
+            });
+
+            messageArray.push("=====================================");
+
+            //Log the message array (sperated by a new line for each item in the array) to the console and text file
+            logMessage(messageArray.join("\n"));
+        })
+        .catch(function (err) {
+            //Log the error
+            logMessage(err);
+        });
 }
 
+//Function to run when the "movie-this" command is used
 function movieThis() {
 
 }
 
+//Function to run when the "do-what-it-says" command is used
 function doWhatItSays() {
 
+}
+
+//Function to sort spotify songs by song name
+function spotifyItemSortBySongName(itemA, itemB) {
+
+    var nameA = itemA.name.toUpperCase(); // ignore upper and lowercase
+    var nameB = itemB.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  
+    // names must be equal
+    return 0;
 }
